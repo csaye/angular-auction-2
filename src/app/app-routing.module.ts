4b49c32bfe54c83af73b5f00dd926ea07f654360
Cooper Saye <coopersaye@gmail.com>
2021-01-15 23:58:54 -0800

    Create product components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'products/:productId',
+    loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
   }
 ];
 
