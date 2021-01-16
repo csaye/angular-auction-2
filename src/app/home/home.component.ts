ff29888e9869c848548576c4e625058ecbae66aa
Cooper Saye <coopersaye@gmail.com>
2021-01-15 17:56:55 -0800

    Implement home component.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MediaObserver } from '@angular/flex-layout';
+import { Product, ProductService } from '../shared/services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nga-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  readonly columns$: Observable<number>;
+  readonly products$: Observable<Product[]>;
+
+  readonly breakpointsToColumnsNumber = new Map([
+    ['xs', 1],
+    ['sm', 2],
+    ['md', 3],
+    ['lg', 4],
+    ['xl', 5]
+  ]);
+
+  constructor(private media: MediaObserver, private productService: ProductService) {
+    this.products$ = this.productService.getAll();
+    this.columns$ = this.media.media$
+    .pipe(
+      map(mc => <number>this.breakpointsToColumnsNumber.get(mc.mqAlias))
+    );
+  }
+
+  ngOnInit(): void {
+  }
+}
